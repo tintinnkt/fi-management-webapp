8d35a2d694fef8818be6d3c02273b38f8657aedd
Krittin Tragunejindarat <itin752@gmail.com>
2023-08-01 17:02:21 +0700

    Create Adding.jsx

---
+++ src/pages/Income Expense feature/Addind income and expense/Adding.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Adding.css'
+
+//component
+import Topic from '../../../components/topic/topic'
+import NavigationBar from '../../../components/navbar/nav'
+
+function Adding() {
+    return (
+        <>
+            <Topic text="Add Income & Expense"/>
+            <div id="container">
+
+                <div className="Howmuch">How much ?</div>
+                <input className="money" type="number"  />
+                <div className="type"><a href=""><i className="bi bi-arrow-down-circle"></i></a></div>
+                <div className="Date">Date</div>
+                <div className="calender"></div>
+
+            </div>
+
+            <button>Record</button>
+            <NavigationBar />
+        </>
+    )
+}
+
+// export default Adding
