d1d8413c54d88d5f75b165b28b722f5d76ddcd80
Krittin Tragunejindarat <itin752@gmail.com>
2023-08-05 23:29:20 +0700

    Add Selector in <Adding/> (CSS required)

---
+++ src/components/selector/AddSelector.jsx
@@ -0,0 +1,55 @@
+
+
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+function AddSelector() {
+    const selector = [
+        { id: 1, type: 'need', text: 'Food & drinks' },
+        { id: 2, type: 'need', text: 'Transportation' },
+        { id: 3, type: 'need', text: 'Electricity & water' },
+        { id: 4, type: 'need', text: 'Phone & internet' },
+        { id: 5, type: 'need', text: 'Housing' },
+        { id: 6, type: 'want', text: 'Shopping' },
+        { id: 7, type: 'want', text: 'Hobby' },
+        { id: 8, type: 'want', text: 'Hangout' },
+        { id: 9, type: 'want', text: 'Travelling' },
+        { id: 10, type: 'want', text: 'Activity' },
+        { id: 11, type: 'want', text: 'Collection' },
+        { id: 12, type: 'income', text: 'Salary' },
+        { id: 13, type: 'income', text: 'Earning' },
+        { id: 14, type: 'income', text: 'Payee' },
+    ];
+
+    const groupedOptions = [
+        {
+            label: 'Need expense',
+            options: selector.filter(item => item.type === 'need').map(item => ({ value: item.text, label: item.text }))
+        },
+        {
+            label: 'Want expense',
+            options: selector.filter(item => item.type === 'want').map(item => ({ value: item.text, label: item.text }))
+        },
+        {
+            label: 'Income',
+            options: selector.filter(item => item.type === 'income').map(item => ({ value: item.text, label: item.text }))
+        }
+    ];
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleSelect = (selectedOption) => {
+        setSelectedOption(selectedOption);
+    };
+
+    return (
+        <Select
+            options={groupedOptions}
+            value={selectedOption}
+            onChange={handleSelect}
+            placeholder="Select an option"
+        />
+    );
+}
+
+export default AddSelector;
