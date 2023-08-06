6f0dca2ef57633a15231de1d87b8ecbc6e21ffe3
Krittin Tragunejindarat <itin752@gmail.com>
2023-08-06 16:08:31 +0700

    Progressbar shown

--- src/components/card/savingcard.jsx
+++ src/components/card/savingcard.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import './card.css'
+import React from 'react';
+import { ProgressBar } from 'react-bootstrap';
+import './card.css';
 
-function Savingcard({prop}) {
+function Savingcard({ prop }) {
   const piggyUrl = "https://cdn-icons-png.flaticon.com/512/9030/9030761.png";
+  const percent = (prop.current / prop.goal * 100).toFixed(1);
+
   return (
     <div className="savecard">
-        <img className="img"  src={piggyUrl} />
-        <div className="Name">Name: <span>{prop.name}</span></div>
-        <div className="Goal">Goal : {prop.goal}</div>
-        <div className="Recent">Current : {prop.current}</div>
-        <div className="percent"><span>{(prop.current/prop.goal * 100).toFixed(1)}</span>%</div>
+      <img className="img" src={piggyUrl} />
+      <div className="Name">Name: {prop.name}</div>
+      <div className="perc">
+        <div className="bar"><ProgressBar now={percent} label={`${percent}%`} /></div>
+        <div className="p-label">{percent}%</div>
+      </div>
+      <div className="Goal">Goal : {prop.goal}</div>
+      <div className="Recent">Current : {prop.current}</div>
     </div>
-  )
+  );
 }
 
-export default Savingcard
+export default Savingcard;
